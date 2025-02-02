796590496f44ea7244d22912acb22210df45dadb
JanJared58 <janjaredrmz32@gmail.com>
2025-02-02 01:29:30 -0600

    ej2

---
+++ Ejercicio 2.py
@@ -0,0 +1,13 @@
+num1 = int(input("Ingrese el primer numero: "))
+num2 = int(input("Ingrese el segundo numero: "))
+
+if num1>num2:
+    print("El numero "+ str(num1)+ " es mayor que " + str(num2) )
+
+elif num1<num2:
+    print("El numero "+ str(num1) + " es menor que " + str(num2) )
+
+else:
+    print("Los numeros son iguales.")
+
+
