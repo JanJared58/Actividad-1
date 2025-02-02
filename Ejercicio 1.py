501d5c13cd4647fc62739bd0a2c0bc6d2593c650
JanJared58 <janjaredrmz32@gmail.com>
2025-02-02 01:20:29 -0600

    Ej1

---
+++ Ejercicio 1.py
@@ -0,0 +1,8 @@
+num1= int(input("Inserte el primer numero: "))
+num2= int(input("Inserte el segundo numero: "))
+
+print("suma: " + str(num1 + num2))
+print("resta: " + str(num1 - num2))
+print("multiplicacion: "+ str(num1 * num2))
+print("division: "+ str(num1/num2))
+print("modulo: "+ str(num1//num2))
