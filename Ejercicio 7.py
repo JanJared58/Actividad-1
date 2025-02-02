0b063f4915e765b05d38d4a765544583222cca45
JanJared58 <janjaredrmz32@gmail.com>
2025-02-02 14:41:48 -0600

    Ej7

---
+++ Ejercicio 7.py
@@ -0,0 +1,6 @@
+peso= float(input("Ingrese su peso (kg): "))
+altura= float(input("Ingrese su altura (m): "))
+
+imc= peso/altura**2
+
+print("El Índice de Masa Corporal de la persona es de: "+ str(imc))
