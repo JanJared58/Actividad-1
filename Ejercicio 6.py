b151c74128f5b6dafd1db8c516d806b9f9d9d340
JanJared58 <janjaredrmz32@gmail.com>
2025-02-02 14:34:51 -0600

    Ej6

---
+++ Ejercicio 6.py
@@ -0,0 +1,8 @@
+celcius= float(input("Ingrese los grados celcius: "))
+
+F= ((celcius*9)/5) + 32
+K= celcius + 274.15
+
+print("Conversion a grados Fahrenheit: " + str(F))
+
+print("Conversion a grados Kelvin: "+ str(K))
