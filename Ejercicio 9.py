63966bc61f2f81a884a4c4bc4ed0528b0ba121dd
JanJared58 <janjaredrmz32@gmail.com>
2025-02-02 15:11:13 -0600

    Ej9

---
+++ Ejercicio 9.py
@@ -0,0 +1,7 @@
+cal1= int(input("Ingrese la calificacion 1: "))
+cal2= int(input("Ingrese la calificacion 2: "))
+cal3= int(input("Ingrese la calificacion 3: "))
+
+promedio = (cal1 + cal2 + cal3)/3
+
+print(f"El promedio es de: {promedio}")
